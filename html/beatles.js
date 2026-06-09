018678066cb670010ce03e8ab69f6ce7d8fc6438
Dante <gonelladantejose@gmail.com>
2026-06-09 19:22:40 -0300

    Entrega html para profe Nico

---
+++ html/beatles.js
@@ -0,0 +1,10 @@
+console.log ("Hola Mundo");
+
+
+let gusto = prompt("Del 1 al 10, ¿cuánto te gustan The Beatles?");
+
+if (gusto === "10") {
+    alert("Felicitaciones! Tu gusto es superior al de los demas!");
+} else {
+    alert("No eres un verdadero fan.");
+}
